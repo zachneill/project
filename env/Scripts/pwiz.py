b51446efd96d89b5814b06b4c57564afbc825252
Zach Neill <neillz@berea.edu>
2021-07-28 01:22:32 -0400

    init commit

---
+++ env/Scripts/pwiz.py
@@ -0,0 +1,4 @@
+#!"C:\Users\ZN\Desktop\WEB DEV\project\env\Scripts\python.exe"
+# EASY-INSTALL-SCRIPT: 'peewee==3.14.4','pwiz.py'
+__requires__ = 'peewee==3.14.4'
+__import__('pkg_resources').run_script('peewee==3.14.4', 'pwiz.py')
